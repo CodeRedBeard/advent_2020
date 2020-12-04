8b0b65745f6f8a5ea8e0bd5fd84adc98283e54ec
Andy Campbell <andy.campbell@gmail.com>
2020-12-03 16:23:12 -0800

    Ignore node_modules

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 import * as Day1 from './day1/day1';
 
-Day1.day1();
+const DAY_NUM: number = 1;
 
+console.log(`Day ${DAY_NUM}`);
+switch (DAY_NUM) {
+    case 1:
+        Day1.day1();
+        break;
+}
