11e26ef88edff169edbadc729a994914356e25db
Andy Campbell <andy.campbell@gmail.com>
2020-12-04 09:01:17 -0800

    day4 validation

--- day4/day4.ts
+++ day4/day4.ts
@@ -2,6 +2,10 @@ function count<T>(arr: T[], f: (x:T) => boolean) {
   return arr.reduce((p, c) => p + (f(c)?1:0), 0);
 }
 
+function notEmpty(x: string): boolean {
+  return (x.length > 0);
+}
+
 type Passport = Map<string, string>;
 
 function parsePassports(fileData: string) {
@@ -16,7 +20,7 @@ function parsePassports(fileData: string) {
       if (line.length === 0) {
         break;
       }
-      let items = line.trim().split(' ');
+      let items = line.trim().split(' ').filter(notEmpty);
       for (const i of items) {
       	let [k,v] = i.split(':');
       	all.set(k, v);
@@ -25,7 +29,7 @@ function parsePassports(fileData: string) {
     return all;
   }
 
-  let fileLines = fileData.split('\n').filter(x => x.length > 0);
+  let fileLines = fileData.split('\n').filter(notEmpty);
   let passes = [];
   while (true) {
   	let passport = eatOne(fileLines);
@@ -38,18 +42,39 @@ function parsePassports(fileData: string) {
 }
 
 function validPassport(pass: Passport): boolean {
-  const checkFields = [
-    ['byr', (x: string) => x.length === 4 && Number(x) >= 1920 && Number(x) <= 2002],
-    ['iyr', (x: string) => x.length === 4 && Number(x) >= 2010 && Number(x) <= 2020],
-    ['eyr', (x: string) => x.length === 4 && Number(x) >= 2020 && Number(x) <= 2030],
-    ['hgt', (x: string) => false],
-    ['hcl', (x: string) => x.match(new RegExp('\#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]'))],
-    ['ecl', (x: string) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(x)],
-    ['pid', (x: string) => x.length === 9 && Array.from(x).every(c => c.match()))],
+  const hclRegEx = new RegExp('[#][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]');
+  const digitRegEx = new RegExp('[0-9]');
+  const eyes =['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+  function checkNum(val: string, len: number, min: number, max: number) {
+  	return val.length === len
+  	  && Number(val) >= min
+  	  && Number(val) <= max;
+  }
+  type Validator = [string, (x:string)=>boolean];
+  const checkFields: Validator[] = [
+    ['byr', (x: string) => checkNum(x, 4, 1920, 2002)],
+    ['iyr', (x: string) => checkNum(x, 4, 2010, 2020)],
+    ['eyr', (x: string) => checkNum(x, 4, 2020, 2030)],
+    ['hgt', (x: string) => {
+    	let suffix = x.substring(x.length - 2);
+    	let num = x.substring(0, x.length - 2);
+    	return (suffix === 'in' && checkNum(num, 2, 59, 76))
+    	 || (suffix === 'cm' && checkNum(num, 3, 150, 193));
+    }],
+    ['hcl', (x: string) => hclRegEx.test(x)],
+    ['ecl', (x: string) => eyes.includes(x)],
+    ['pid', (x: string) => 
+      x.length === 9 
+      && Array.from(x).every(c => digitRegEx.test(x))],
     //'cid',
   ];
   for (const field of checkFields) {
-    if (!pass.has(field[0])) {
+    let passField = pass.get(field[0])
+    if (passField === undefined) {
+      return false;
+    }
+    let func = field[1];
+    if (!func(passField)) {
       return false;
     }
   }
