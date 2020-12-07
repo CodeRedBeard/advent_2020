09ed273dc390dc08abdd32dad3be57ff0685d696
Andy Campbell <andy.campbell@gmail.com>
2020-12-06 22:05:00 -0800

    day7 part1 more

--- day7/day7.ts
+++ day7/day7.ts
@@ -1,3 +1,4 @@
+type BagContent = [number,string];
 function readRules(lines: string[]) {
   function readBagColor(arr: string[]) {
     return `${arr[0]}_${arr[1]}`;
@@ -8,23 +9,55 @@ function readRules(lines: string[]) {
     x.shift();
     return [count, color];
   }
-  return lines.map(line => {
+  return new Map<string, BagContent[]>(lines.map(line => {
     let terms = line.split(' ');
     let color = readBagColor(terms);
     terms = terms.slice(4);
     if (terms[0] === 'no') {
       return [];
     }
-    let contents = [];
+    let contents: BagContent[] = [];
     while (terms.length > 0) {
       contents.push(readContents(terms));
     }
-    return contents;
+    return [color, contents];
   });
+}
+
+function invertRules(rules: Map<string,[number,string][]>) {
+  let outers = new Map<string, string[]>();
+  for (let [k, v] of rules.entries()) {
+    for (let vx of v) {
+      let color = vx[1];
+      let o = outers.get(color) ?? [];
+      o.push(k);
+      outers.set(color, o);
+    }
+  }
+}
+
+function countOuters(search: string, outers: Map<string,string>) {
+  let num = 0;
+  let q = [search];
+  let v = new Set<string>();
+  while (q.length > 0) {
+    let x = q.pop();
+    v.add(x);
+    let o = outers.get(x);
+    for (let i of o) {
+      if (!v.has(i)) {
+        q.push(i);
+        ++num;
+      }
+    }
+  }
+  return num;
 }
 
 export function run(fileData: string) {
   let lines = fileData.split('\n');
 
   let rules = readRules(lines);
+  let outers = invertRules(rules);
+  
 }
