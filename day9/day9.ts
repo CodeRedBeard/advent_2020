f8f94850d7a0a20bed8133288ba8a8ff553d4df8
Andy Campbell <andy.campbell@gmail.com>
2020-12-08 22:13:23 -0800

    day9 part2 success

--- day9/day9.ts
+++ day9/day9.ts
@@ -24,9 +24,29 @@ function solvePart1(lines: number[]) {
   return undefined;
 }
 
+function solvePart2(lines: number[]) {
+  const badVal = solvePart1(lines);
+  for (let start = 0; start < lines.length; ++start){
+    let sum = 0;
+    let x = start;
+    for (x = start; sum < badVal; ++x) {
+      sum += lines[x];
+    }
+    if (sum === badVal) {
+      let range = lines.slice(start, x);
+      let min = Math.min(...range);
+      let max = Math.max(...range);
+      return min + max;
+    }
+  }
+}
+
 export function run(fileData: string) {
   let lines = fileData.split('\n').map(Number);
 
   let result_1 = solvePart1(lines);
   console.log(`Part1: ${result_1}`);
+
+  let result_2 = solvePart2(lines);
+  console.log(`Part2: ${result_2}`);
 }
