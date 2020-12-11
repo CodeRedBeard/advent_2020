966ba4c31d07c400b3ff0b305ec74a6df3f430e3
Andy Campbell <andy.campbell@gmail.com>
2020-12-10 23:05:07 -0800

    day11 part2 success

--- day11/day11.ts
+++ day11/day11.ts
@@ -1,4 +1,4 @@
-import { notEmpty, count } from '../util';
+import { notEmpty, countOf, countSum } from '../util';
 import { TestCase, test } from '../test';
 
 const testCases1 : TestCase<string[],number>[]= [
@@ -14,6 +14,9 @@ const testCases1 : TestCase<string[],number>[]= [
     'L.LLLLLL.L',
     'L.LLLLL.LL',
   ], 37],
+];
+const testCases2 : TestCase<string[],number>[]= [
+  [testCases1[0][0], 26],
 ];
 
 enum Cell {
@@ -33,8 +36,27 @@ function makeCell(x: string): Cell {
   }
   throw new Error();
 }
+function printCell(c: Cell): string {
+  switch (c) {
+    case Cell.Floor: return '.';
+    case Cell.Empty: return 'L';
+    case Cell.Taken: return '#';
+  }
+}
 class Grid {
-  cells: Cell[][] = [];
+  private readonly cells: Cell[][] = [];
+
+  public get height() { return this.cells.length; }
+  public get width() { return this.cells[0].length; }
+
+  private constructor(cells: Cell[][]) {
+    this.cells = cells;
+  }
+
+  public static create(lines: string[]) {
+    return new Grid(lines.map(
+      line => line.split('').map(makeCell)));
+  }
 
   public getCell(x: number, y: number): Cell {
     return this.cells[y][x];
@@ -44,48 +66,61 @@ class Grid {
     this.cells[y][x] = c;
   }
 
-  public getAdjacents(xPos: number, yPos: number) {
+  public getVisibles(xPos: number, yPos: number, maxDist: number) {
+    let dirs = [
+      [-1,-1],
+      [ 0,-1],
+      [+1,-1],
+      [-1, 0],
+      [+1, 0],
+      [-1,+1],
+      [ 0,+1],
+      [+1,+1],
+    ]
     let adj: Cell[] = [];
-    for (let y = yPos-1; y <= yPos+1; ++y) {
-      if (y < 0 || y >= this.cells.length) {
-        continue;
-      }
-      const cellLine = this.cells[y];
-      for (let x = xPos-1; x <= xPos+1; ++x) {
-        if (x < 0 || x >= cellLine.length) {
-          continue;
+    for (const [dx,dy] of dirs) {
+      for (let dist = 1; dist <= maxDist; ++dist) {
+        let x = xPos + dx * dist;
+        let y = yPos + dy * dist;
+        if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+          break;
         }
-        if (x === xPos && y === yPos) {
-          continue;
+        const cell = this.getCell(x,y);
+        if (dist >= maxDist || cell !== Cell.Floor) {
+          adj.push(cell);
+          break;
         }
-        adj.push(cellLine[x]);
       }
     }
     return adj;
   }
 
   public numTaken(): number {
-    return this.cells.reduce(
-      (sum,line) => sum + count(line,
-        c => c === Cell.Taken),
-      0);
+    return countSum(this.cells, 
+      (line) => countOf(line, Cell.Taken));
   }
 
+  public drawGrid() {
+    for (let y = 0; y < this.height; ++y) {
+      const line = this.cells[y].map(printCell).join('');
+      console.log(line);
+    }
+  }
+  
   public clone(): Grid {
-    let newGrid = new Grid();
-    newGrid.cells = this.cells.map(x => x.slice());
+    let newGrid = new Grid(this.cells.map(x => x.slice()));
     return newGrid;
   }
 }
 
-function runRules(oldGrid: Grid): [Grid,number] {
+function runRules(oldGrid: Grid, maxVisible: number, maxDist: number): [Grid,number] {
   let newGrid = oldGrid.clone();
   let changed = 0;
-  for (let [y, cellLine] of oldGrid.cells.entries()) {
-    for (let [x, cell] of cellLine.entries()) {
-      let adj = oldGrid.getAdjacents(x,y);
-      const adjTaken = count(adj,
-        a => a === Cell.Taken);
+  for (let y = 0; y < oldGrid.height; ++y) {
+    for (let x = 0; x < oldGrid.width; ++x) {
+      const cell = oldGrid.getCell(x,y);
+      let adj = oldGrid.getVisibles(x,y,maxDist);
+      const adjTaken = countOf(adj, Cell.Taken);
       if (cell === Cell.Empty) {
         if (adjTaken === 0) {
           newGrid.setCell(x,y,Cell.Taken);
@@ -93,7 +128,7 @@ function runRules(oldGrid: Grid): [Grid,number] {
         }
       }
       if (cell === Cell.Taken) {
-        if (adjTaken >= 4) {
+        if (adjTaken >= maxVisible) {
           newGrid.setCell(x,y,Cell.Empty);
           ++changed;
         }
@@ -104,13 +139,26 @@ function runRules(oldGrid: Grid): [Grid,number] {
 }
 
 function solvePart1(lines: string[]) {
-  let grid = new Grid();
-  grid.cells = lines.map(
-    line => line.split('').map(makeCell));
+  let grid = Grid.create(lines);
   while (true) {
-    let [newGrid,changed] = runRules(grid);
-    console.log(`Taken: ${newGrid.numTaken()}`);
-    console.log(`Changed: ${changed}`);
+    let [newGrid,changed] = runRules(grid, 4, 1);
+    //newGrid.drawGrid();
+    //console.log(`Taken: ${newGrid.numTaken()}`);
+    //console.log(`Changed: ${changed}`);
+    if (changed === 0) {
+      return newGrid.numTaken();
+    }
+    grid = newGrid;
+  }
+}
+
+function solvePart2(lines: string[]) {
+  let grid = Grid.create(lines);
+  while (true) {
+    let [newGrid,changed] = runRules(grid, 5, Number.POSITIVE_INFINITY);
+    //newGrid.drawGrid();
+    //console.log(`Taken: ${newGrid.numTaken()}`);
+    //console.log(`Changed: ${changed}`);
     if (changed === 0) {
       return newGrid.numTaken();
     }
@@ -120,8 +168,12 @@ function solvePart1(lines: string[]) {
 
 export function run(fileData: string) {
   test(solvePart1, testCases1);
+  test(solvePart2, testCases2);
   let lines = fileData.split('\n').filter(notEmpty);
 
   let result_1 = solvePart1(lines);
-  console.log(`Part1: (seats taken): ${result_1}`);
+  console.log(`Part1: (adjacent): ${result_1}`);
+
+  let result_2 = solvePart2(lines);
+  console.log(`Part2: (visible): ${result_2}`);
 }
--- util.ts
+++ util.ts
@@ -6,6 +6,14 @@ export function count<T>(arr: T[], f: (x: T) => boolean) {
     return arr.reduce((p, c) => p + (f(c) ? 1 : 0), 0);
 }
 
+export function countSum<T>(arr: T[], f: (x: T) => number) {
+    return arr.reduce((p, c) => p + f(c), 0);
+}
+
+export function countOf<T>(arr: T[], val: T) {
+    return arr.reduce((p, c) => p + ((c === val) ? 1 : 0), 0);
+}
+
 export function sum(a: number, b: number): number {
     return a + b;
 }
