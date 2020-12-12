e5131f916cbaa7eef2426f240b1e2ef2477367ad
Andy Campbell <andy.campbell@gmail.com>
2020-12-11 22:53:26 -0800

    day12 part2 success

--- day12/day12.ts
+++ day12/day12.ts
@@ -1,3 +1,5 @@
+import { FTestCase, testFuncs } from "../test";
+
 type Instruc = [string,number];
 function parseInstruc(lines: string[]) {
   let instr: Instruc[] = lines.map(
@@ -38,8 +40,58 @@ function solvePart1(lines: string[]) {
   return Math.abs(pos.x) + Math.abs(pos.y);
 }
 
+function solvePart2(lines: string[]) {
+  let instr = parseInstruc(lines);
+  let pos = {x:0, y:0};
+  let wp = {x:10, y:1};
+  for (const i of instr) {
+    let val = i[1];
+    switch (i[0]) {
+      case 'L': 
+        for (let angle = val; angle > 0; angle -= 90) {
+          let {x,y} = wp;
+          wp.x = -y;
+          wp.y = x;
+        }
+        break;
+      case 'R':
+        for (let angle = val; angle > 0; angle -= 90) {
+          let {x,y} = wp;
+          wp.x = y;
+          wp.y = -x;
+        }
+        break;
+      case 'F':
+        pos.x += wp.x * val;
+        pos.y += wp.y * val;
+        break;
+      case 'N': wp.y += val; break;
+      case 'S': wp.y -= val; break;
+      case 'E': wp.x += val; break;
+      case 'W': wp.x -= val; break;
+    }
+  }
+
+  return Math.abs(pos.x) + Math.abs(pos.y);
+}
+
+const testInput1 = [
+  'F10',
+  'N3',
+  'F7',
+  'R90',
+  'F11',
+];
+const testCases: FTestCase<string[], number>[] = [
+  [solvePart1, testInput1, 25],
+  [solvePart2, testInput1, 286],
+];
+
 export function run(fileData: string) {
+  testFuncs(testCases);
+
   let lines = fileData.split('\n')
 
   console.log(`Part1: ${solvePart1(lines)}`);
+  console.log(`Part2: ${solvePart2(lines)}`);
 }
--- test.ts
+++ test.ts
@@ -1,7 +1,18 @@
 export type TestCase<T,Y> = [T,Y];
+export type FTestCase<T,Y,F = (t: T) => Y> = [F,T,Y];
 
 export function test<T, Y>(f: (t: T) => Y, cases: TestCase<T,Y>[]) {
   for (const [idx, [vIn, vOut]] of cases.entries()) {
+    let v = f(vIn);
+    if (vOut !== v) {
+      throw new Error(
+        `case [${idx}]: ${vOut} != ${v} = f(${vIn})`);
+    }
+  }
+}
+
+export function testFuncs<T, Y, F extends (t: T) => Y>(cases: FTestCase<T,Y,F>[]) {
+  for (const [idx, [f, vIn, vOut]] of cases.entries()) {
     let v = f(vIn);
     if (vOut !== v) {
       throw new Error(
