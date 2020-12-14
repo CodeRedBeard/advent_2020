54e1a41529e86cb1e97f032a382cf003891be290
Andy Campbell <andy.campbell@gmail.com>
2020-12-13 20:37:25 -0800

    day13 part2 success

--- day13/day13.ts
+++ day13/day13.ts
@@ -15,6 +15,21 @@ function solvePart1(lines: string[]) {
   }
 }
 
+function solvePart2(lines: string[]) {
+  let routes = lines[1].split(',')
+    .map((x, idx) => [Number(x), idx])
+    .filter(x => !isNaN(x[0]));
+  let t = 0;
+  let harmonic = 1;
+  for (const [freq,offset] of routes) {
+    while ((t + offset) % freq !== 0) {
+      t += harmonic;
+    }
+    harmonic *= freq;
+  }
+  return t;
+}
+
 const testCases: FTestCase<string[],number>[] = [
   [ solvePart1,
     [ '939',
@@ -22,10 +37,30 @@ const testCases: FTestCase<string[],number>[] = [
     ],
     295,
   ],
+  [ solvePart2,
+    [ 'x',
+      '7,13,x,x,59,x,31,19',
+    ],
+    1068781,
+  ],
+  [ solvePart2,
+    [ 'x',
+      '17,x,13,19',
+    ],
+    3417,
+  ],
+  [ solvePart2,
+    [ 'x',
+      '67,7,59,61',
+    ],
+    754018,
+  ],
 ]
+
 export function run(fileData: string) {
   testFuncs(testCases);
   let lines = fileData.split('\n');
 
   console.log(`Part1: ${solvePart1(lines)}`);
+  console.log(`Part2: ${solvePart2(lines)}`);
 }
