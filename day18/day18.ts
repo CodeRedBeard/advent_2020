c3bed06aef44f260fd9bcc59c68d1cd3857b57c1
Andy Campbell <andy.campbell@gmail.com>
2020-12-18 03:14:00 -0800

    day18 part2

--- day18/day18.ts
+++ day18/day18.ts
@@ -2,8 +2,12 @@ import { sum, notEmpty } from '../util';
 import { testFuncs, FTestCase } from '../test';
 
 type Operator = '+' | '*' | '(';
+interface Precedence {
+  ['+']: number;
+  ['*']: number;
+}
 
-function evalExpr(str: string): number {
+function evalExpr(str: string, prec: Precedence): number {
   let rpn: (number | Operator)[] = [];
   let opStack: Operator[] = [];
   for (let c of str) {
@@ -17,9 +21,15 @@ function evalExpr(str: string): number {
         break;
       case '+':
       case '*':
-        while (opStack.length > 0
-          && opStack[opStack.length-1] !== '(') {
-          rpn.push(opStack.pop());
+        while (opStack.length > 0) {
+          let topOp = opStack[opStack.length-1];
+          if (topOp !== '('
+            && prec[topOp] >= prec[c]) {
+            rpn.push(opStack.pop());
+          }
+          else {
+            break;
+          }
         }
         opStack.push(c);
         break;
@@ -60,18 +70,40 @@ function evalExpr(str: string): number {
   return evalStack[0];
 }
 
+function evalExpr1(str: string) {
+  return evalExpr(str, {
+    '*': 1,
+    '+': 1,
+  });
+}
+
+function evalExpr2(str: string) {
+  return evalExpr(str, {
+    '*': 1,
+    '+': 2,
+  });
+}
+
 function solvePart1(lines: string[]) {
-  let values = lines.map(evalExpr);
+  let values = lines.map(evalExpr1);
+  return values.reduce(sum);
+}
+
+function solvePart2(lines: string[]) {
+  let values = lines.map(evalExpr2);
   return values.reduce(sum);
 }
 
 const testCases: FTestCase<string,number>[] = [
-  [ evalExpr,
+  [ evalExpr1,
     '2 * 3 + (4 * 5)',
     26],
-  [ evalExpr,
+  [ evalExpr1,
     '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
     13632],
+  [ evalExpr2,
+    '2 * 3 + (4 * 5)',
+    46],
 ];
 
 export function run(fileData: string) {
@@ -79,4 +111,5 @@ export function run(fileData: string) {
 
   let lines = fileData.split('\n').filter(notEmpty);
   console.log(`Part1: ${solvePart1(lines)}`);
+  console.log(`Part2: ${solvePart2(lines)}`);
 }
