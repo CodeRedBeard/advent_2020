1b45243e290930cd9b2fce0120404c30d895a3b8
Andy Campbell <andy.campbell@gmail.com>
2020-12-19 15:02:22 -0800

    day19 part2 success

--- day19/day19.ts
+++ day19/day19.ts
@@ -5,15 +5,19 @@ function splitTrim(str: string, c: string): string[] {
   return str.split(c).map(x => x.trim());
 }
 
-function parseRules(input: string) {
+function parseRules(input: string, overrides: string[]) {
+  function getLineRule(line: string): [number, string[][]] {
+    let [idx,rest] = splitTrim(line, ':');
+    let options = splitTrim(rest, '|')
+      .map(x => splitTrim(x, ' '));
+    return [Number(idx), options];
+  }
   let lines = input.split('\n\n')[0].split('\n');
-  let rules = new Map(lines.map(
-    line => {
-      let [idx,rest] = splitTrim(line, ':');
-      let options = splitTrim(rest, '|')
-        .map(x => splitTrim(x, ' '));
-      return [Number(idx), options];
-    }));
+  let rules = new Map(lines.map(getLineRule));
+  let overrideRules = overrides.map(getLineRule);
+  for (const override of overrideRules) {
+    rules.set(override[0], override[1]);
+  }
   //console.log(rules.entries())
   function makeExpr(idx: number): string {
     let options = rules.get(idx);
@@ -39,7 +43,18 @@ function testLines(regex: RegExp, input: string) {
 }
 
 function solvePart1(input: string) {
-  let rules = parseRules(input);
+  let rules = parseRules(input, []);
+  //console.log(rules);
+  return testLines(rules, input);
+}
+
+function solvePart2(input: string) {
+  // This is a horrible hack, but it's enough for the given inputs
+  let rules = parseRules(input,
+    [
+      '8: 42 | 42 42 | 42 42 42 | 42 42 42 42 | 42 42 42 42 42 | 42 42 42 42 42 42 | 42 42 42 42 42 42 42 | 42 42 42 42 42 42 42 42',
+      '11: 42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31 | 42 42 42 42 42 42 31 31 31 31 31 31',
+    ]);
   //console.log(rules);
   return testLines(rules, input);
 }
@@ -48,10 +63,14 @@ const testCases: FTestCase<string,number>[] = [
   [ solvePart1,
     readInputFile('day19/test1.txt'),
     2 ],
+  [ solvePart2,
+    readInputFile('day19/test2.txt'),
+    12 ],
 ];
 
 export function run(fileData: string) {
   testFuncs(testCases);
 
   console.log(`Part1: ${solvePart1(fileData)}`);
+  console.log(`Part2: ${solvePart2(fileData)}`);
 }
