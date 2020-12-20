ce2ac914edcd6578a10e9f3c0001dbcf87bd33f3
Andy Campbell <andy.campbell@gmail.com>
2020-12-20 13:45:20 -0800

    day20 part2 start

--- day20/day20.ts
+++ day20/day20.ts
@@ -20,31 +20,32 @@ function getEdges(tile: string[]) {
   ];
 }
 
-function solvePart1(input: string) {
+function findTileEdges(input: string) {
   let tiles = readTiles(input);
-  let edges = tiles.map(t => ({
+  //console.log(`num tiles: ${tiles.length}`);
+  let tileEdges = tiles.map(t => ({
     id: Number(t.id),
     edges: getEdges(t.data)}));
   //console.log(edges);
-  let edgeMap = new Map<string,number[]>();
-  function addEdge(e: string, id: number) {
+  let edgeMap = new Map<string,[number,number][]>();
+  function addEdge(e: string, id: number, idx: number) {
     let list = edgeMap.get(e);
     if (!list) {
       list = [];
       edgeMap.set(e, list);
     }
-    list.push(id);
+    list.push([id,idx]);
   }
-  for (let t of edges) {
-    for (let e of t.edges) {
-      addEdge(e, t.id);
-      addEdge(Array.from(e).reverse().join(''), t.id);
+  for (let t of tileEdges) {
+    for (let [idx,e] of t.edges.entries()) {
+      addEdge(e, t.id, idx);
+      addEdge(Array.from(e).reverse().join(''), t.id, idx);
     }
   }
   //console.log(edgeMap.entries());
   let singles = Array.from(edgeMap.entries())
     .filter(x => x[1].length === 1)
-    .map(x => x[1][0]);
+    .map(x => x[1][0][0]);
   let counts = new Map<number,number>();
   for (let x of singles) {
     let num = counts.get(x) ?? 0;
@@ -57,13 +58,41 @@ function solvePart1(input: string) {
   if (corners.length != 4) {
     throw new Error();
   }
-  return corners.reduce(mul);
+  return {
+    tiles: tiles,
+    edges: tileEdges,
+    corners: corners,
+  };
+}
+
+function layoutImage(tiles: ReturnType<typeof findTileEdges>) {
+  let numTilesWide = Math.sqrt(tiles.tiles.length);
+  let tileWidth = tiles.tiles[0].data[0].length - 2;
+  let imgWidth = tileWidth * numTilesWide;
+  console.log(`imgsize:${imgWidth}^2`);
+  return {
+    
+  };
+}
+
+function solvePart1(input: string) {
+  let tiles = findTileEdges(input);
+  return tiles.corners.reduce(mul);
+}
+
+function solvePart2(input: string) {
+  let tiles = findTileEdges(input);
+  let image = layoutImage(tiles);
+  return 0;
 }
 
 const testCases: FTestCase<string,number>[] = [
   [ solvePart1,
     readInputFile('day20/test1.txt'),
     20899048083289 ],
+  [ solvePart2,
+    readInputFile('day20/test1.txt'),
+    273 ],
 ];
 
 export function run(fileData: string) {
